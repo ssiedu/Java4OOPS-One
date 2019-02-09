b1e5b3801e41b0d96ad3c13de238b12b5bc26fe2
manoj <manoj.sarwate@ssiedu.in>
2019-02-09 17:25:51 +0530

    09-02-19

--- src/Demo.java
+++ src/Demo.java
@@ -2,6 +2,12 @@ public class Demo {
 
     private int x, y, z;
     
+    public Demo(Demo tmp){
+        x=tmp.x;
+        y=tmp.y;
+        z=tmp.z;
+    }
+    
     public Demo(int p, int q){
         x=p; y=q;
     }
@@ -19,9 +25,12 @@ public static void main(String[] args) {
    
     Demo d1=new Demo(10,20,30);
     Demo d2=new Demo(40,50);
+    
+    Demo d3=new Demo(d2);
+    
     d1.showData();
     d2.showData();
-   
+    d3.showData();
     
 }
     
